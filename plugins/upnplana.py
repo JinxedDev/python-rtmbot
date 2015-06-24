717d77d2df74bffdbeee31a016cd9f47ea24996c
Sean Macfarlane <s.n.macfarlane@gmail.com>
2015-06-24 21:33:23 +1000

    'consistent'

--- plugins/upnplana.py
+++ plugins/upnplana.py
@@ -6,7 +6,7 @@ outputs = []
 
 def process_message(data):
 	if 'type' in data:
-		if data['type'] == 'message' and 'UPNP' in data['text'].upper():
+		if data['type'] == 'message' and re.search(r'upnp', data['text'], re.IGNORECASE) is not None:
 			outputs.append([data['channel'], 'https://www.youtube.com/watch?v=xn6hhrX34Pw'])
 		elif data['type'] == 'message' and re.search(r'lana', data['text'], re.IGNORECASE) is not None:
 			outputs.append([data['channel'], 'YUUP!'])
